226138e1c0612642eb469497e948598133da91cc
Mikita Belahlazau <nikelandjelo@gmail.com>
2023-08-20 15:54:31 -0700

    Add diff mode

--- python/README.md
+++ python/README.md
@@ -21,3 +21,30 @@ python setup.py develop
 pip install -r requirements.txt
 pytest
 ```
+
+### Diff mode
+
+Diff mode helps to see if changes to translation algorithms have any effect. it is useful during development, especially while we are working on an existing algorithm that we don't fully understand yet. Steps to use it:
+
+1. Guard changes using `diff_mode.is_new()` condition. For example in order to test whether removal of some conversion affects results:
+
+```python
+if not diff_mode.is_new():
+    res = res.replace('сллі', 'ślli')
+```
+
+2. Run diff tool:
+
+```
+python diff.py
+```
+
+It will go through a set of Belarusian texts, converting each into Lacinka twice: once with `is_new()` being set to False (golden) and once with it set to True (test). If there are changes - they will be printed an a html file with nice handy UI.
+
+3. Run tests:
+
+```
+DIFF_NEW=true pytest
+```
+
+This will run unit tests with `is_new()` being set to true. This helps to quickly see if the change affects existing tests. If you change affects a set of sample texts from step 2 but doesn't affect any unit tests - consider adding a new unit test.
---
+++ python/diff.py
@@ -0,0 +1,84 @@
+
+from dataclasses import dataclass
+import itertools
+import os
+import sys
+import tempfile
+import difflib
+
+import requests
+from bs4 import BeautifulSoup
+from translit import engine, diff_mode, cases
+
+@dataclass
+class Sample:
+    """Sample data for testing. Should point to a text or html file on the web."""
+    name: str
+    url: str
+    orthography: cases.Case
+
+SAMPLES = [
+    Sample(
+        name = 'long_way_home',
+        url = 'https://knihi.com/Vasil_Bykau/Douhaja_daroha_dadomu.html',
+        orthography = cases.Case.CYR_TARAS,
+    ),
+    Sample(
+        name = 'charnobylskaja_malitva',
+        url = 'https://knihi.com/Sviatlana_Aleksijevic/Carnobylskaja_malitva.html',
+        orthography = cases.Case.CYR_NAR
+    ),
+]
+
+def maybe_cache_file(name: str, url: str):
+    """If the file is not cached, download it and cache it"""
+    if not os.path.exists(name):
+        with open(name, 'w') as f:
+            r = requests.get(url)
+            html = r.content.decode('utf-8')
+            f.write(BeautifulSoup(html, 'html.parser').get_text())
+
+def read_file_from_url(sample: Sample) -> str:
+    """Read the file from the given URL and return the contents as a string"""
+    full_name = tempfile.gettempdir() + '/' + sample.name + '.txt'
+    maybe_cache_file(full_name, sample.url)
+    return open(full_name, 'r').read()
+
+def process_sample(sample: Sample):
+    cyr = read_file_from_url(sample)
+
+    diff_mode.set_new(False)
+    lac_golden = engine.convert(cyr, sample.orthography, engine.Case.LAT)
+
+    diff_mode.set_new(True)
+    lac_new = engine.convert(cyr, sample.orthography, engine.Case.LAT)
+    if lac_golden == lac_new:
+        print(f"{sample.name}: OK")
+    else:
+        cyr_lines = cyr.splitlines()
+        empty_lines = [''] * len(cyr_lines)
+        golden_lines = lac_golden.splitlines()
+        golden_lines = list(itertools.chain(*zip(empty_lines, cyr_lines, golden_lines)))
+        new_lines = lac_new.splitlines()
+        new_lines = list(itertools.chain(*zip(empty_lines, cyr_lines, new_lines)))
+
+        diff = difflib.HtmlDiff().make_file(golden_lines, new_lines, "Golden", "New")
+        # Remove no wrap and set max with to ~half screen so that lon lines are wrapped.
+        # Also replaces non-breaking spaces with regular spaces to achieve wrapping.
+        diff = diff.replace('nowrap="nowrap"', '')
+        diff = diff.replace('<head>', '<head><style>td { max-width: 45vw; } </style>')
+        diff = diff.replace('&nbsp;', ' ')
+        diff_file = tempfile.gettempdir() + '/' + sample.name + '.diff.html'
+        with open(diff_file, 'w') as f:
+            f.write(diff)
+        print(f"{sample.name}: DIFF file://{diff_file}")
+
+
+
+def main() -> int:
+    for sample in SAMPLES:
+        process_sample(sample)
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
